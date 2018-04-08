5d9170324c197cd0fc41147b0c7e2e1940f2fe70
JonPatel <37722718+JonPatel@users.noreply.github.com>
2018-04-08 11:46:24 -0500

    Add files via upload
    Changes have been made 4/08/2018

--- Sphere.h
+++ Sphere.h
@@ -1,26 +1,35 @@
 #ifndef SPHERE_H_INCLUDED
 #define SPHERE_H_INCLUDED
 
+#include "Point.h"
+#include "Color.h"
+#include "SDL_Plotter.h"
 #include <iostream>
 using namespace std;
 
 //Global Variable
-const double pi = 3.14159;
+const double PI = 3.14159;
 
 class Sphere
 {
     private:
-        radius;
+        double radius;
         Point center;
         Color sphereColor;
 
     public:
         Sphere();  //Default constructor
-        Sphere(center, r);
+        Sphere(Point centerobject, double r); //Constructor with Point object and radius parameters
 
+        //Setters constant
+        void setRadius(const double);
+        void setCenter(const Point);
+        void setColor(const string);
 
-
-
+        //Getters constant
+        double getRadius() const;
+        Point  getCenter() const;
+        Color  getColor() const;
 
 };
 
