7d0a0a8fdc07b893e7798b6d4bbd26c1433736f1
JonPatel <37722718+JonPatel@users.noreply.github.com>
2018-04-09 14:46:40 -0500

    Add files via upload
    Update main and Sphere 4/09/2018

--- Sphere.h
+++ Sphere.h
@@ -5,6 +5,7 @@
 #include "Color.h"
 #include "SDL_Plotter.h"
 #include <iostream>
+
 using namespace std;
 
 //Global Variable
@@ -19,19 +20,113 @@ class Sphere
 
     public:
         Sphere();  //Default constructor
-        Sphere(Point centerobject, double r); //Constructor with Point object and radius parameters
+        Sphere(Point centerObject, double r); //Constructor with Point object and radius parameters
 
         //Setters constant
         void setRadius(const double);
-        void setCenter(const Point);
-        void setColor(const string);
+        void setCenter(const Point&);
+        void setColor(const Color&);
 
         //Getters constant
         double getRadius() const;
         Point  getCenter() const;
         Color  getColor() const;
 
+        void draw(SDL_Plotter&) const;
+        void erase(SDL_Plotter&) const;
+
+        //void moveSphere(char direction);
+
 };
+
+        Sphere::Sphere()  //Default constructor
+        {
+          center = Point(350, 600);
+          radius = 10;
+          sphereColor = Color(220, 20, 20);
+        }
+
+        Sphere::Sphere(Point centerObject, double r) //Constructor with Point object and radius parameters
+        {
+          center = Point(350, 600);
+          r = 10;
+          sphereColor = Color(220, 20, 20);
+        }
+
+        //Setters constant
+        void Sphere::setRadius(const double r)
+        {
+          radius = r;
+        }
+
+        void Sphere::setCenter(const Point& p)
+        {
+          center = p;
+        }
+
+        void Sphere::setColor(const Color& c)
+        {
+          sphereColor = c;
+        }
+
+        //Getters constant
+        double Sphere::getRadius() const
+        {
+          return radius;
+        }
+
+        Point  Sphere::getCenter() const
+        {
+          return center;
+        }
+
+        Color  Sphere::getColor() const
+        {
+          return sphereColor;
+        }
+
+        void Sphere::draw(SDL_Plotter& g) const{ //label plotter g
+    for(int x = -radius; x <= radius; x++){
+        for(int y = -radius; y <= radius;y++){
+            if(sqrt(x*x + y*y <= radius)){
+            g.plotPixel(x + center.x,y + center.y, sphereColor.R, sphereColor.G, sphereColor.B);
+            g.plotPixel(x + center.x,-y + center.y, sphereColor.R, sphereColor.G, sphereColor.B);
+            }
+        }
+    }
+}
+
+void Sphere::erase(SDL_Plotter& g) const{ //label plotter g
+    for(int x = -radius; x <= radius; x++){
+        for(int y = -radius; y <= radius;y++){
+            if(sqrt(x*x + y*y <= radius)){
+            g.plotPixel(x + center.x,y + center.y, 255, 255, 255);
+            }
+        }
+    }
+}
+/*
+void PlayerShip::moveShip(char direction)
+{
+    switch (direction)
+    {
+        case 'w':
+            y--;
+            break;
+        case 's':
+            y++;
+            break;
+        case 'a':
+            x--;
+            break;
+        case 'd':
+            x++;
+            break;
+    }
+}
+*/
+
+
 
 
 #endif // SPHERE_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,31 +1,76 @@
+
+
 #include <iostream>
 
 #include "SDL_Plotter.h"
+#include "Color.h"
+#include "Sphere.h"
+#include "Point.h"
+
 
 using namespace std;
 
 int main(int argc, char ** argv)
 {
 
-    SDL_Plotter g(600,900);
+    SDL_Plotter g(700, 700);
     bool stopped = false;
-    bool colored = false;
-    int x,y, xd, yd;
+    //bool colored = false;
+    int x,y;
     int R,G,B;
+
+    Sphere ship;
+    ship.setCenter(Point(350, 600));
+    ship.setColor(Color(212, 20, 20));
+    ship.setRadius(10);
+
+    Color test;
 
     while (!g.getQuit())
     {
 		if(!stopped)
 		{
-            x = rand()%g.getCol();
-			y = rand()%g.getRow();
-			R = rand()%256;
-			G = rand()%256;
-			B = rand()%256;
+      //x = rand()%g.getCol();
+			//y = rand()%g.getRow();
+      //x = 20;
+			//y = 20;
+
+			//R = rand()%256;
+			//G = rand()%256;
+			//B = rand()%256;
 
 			//g.getCol();
 			//g.getRow();
-			g.plotPixel(x,y,R,G,B);
+			//g.plotPixel(x,y,R,G,B);
+      //test = ship.getColor();
+
+			//cout << test.R << endl;
+
+			ship.draw(g);
+			/*
+char direction;
+if(g.kbhit())
+{
+  cout << g.getKey() << endl;
+
+    switch (direction)
+    {
+        case 'w':
+            y--;
+            break;
+        case 's':
+            y++;
+            break;
+        case 'a':
+            x--;
+            break;
+        case 'd':
+            x++;
+            break;
+    }
+}
+
+*/
 
 			/*
 			for(xd = 0; xd < 10 && x + xd < g.getCol(); xd++ ){
@@ -36,7 +81,6 @@ int main(int argc, char ** argv)
 					else{
 					    g.plotPixel( x + xd, y + yd, 990, G, 990);
 					}
-
 				}
 			}
 			*/
@@ -52,4 +96,5 @@ int main(int argc, char ** argv)
 
 		g.update();
     }
+    return 0;
 }
